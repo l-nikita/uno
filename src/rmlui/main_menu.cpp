c8c631af68b2697565ca326347094ebd0e702dd3
lnikita <121461047+l-nikita@users.noreply.github.com>
2026-02-28 02:29:33 +0200

    Update

--- src/rmlui/main_menu.cpp
+++ src/rmlui/main_menu.cpp
@@ -7,7 +7,7 @@
 MainMenu::MainMenu(Rml::Context* context)
 	: m_context(context)
 {
-	m_document = m_context->LoadDocument("ui/main_menu.rml");
+	m_document = m_context->LoadDocument("ui/rml/main_menu.rml");
 	m_document->Show();
 
 	Rml::ElementList elements;
