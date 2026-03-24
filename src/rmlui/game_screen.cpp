50fa1a548faa68f9246bcc16ac0923692eec34a4
lnikita <121461047+l-nikita@users.noreply.github.com>
2026-03-24 17:26:12 +0200

    Add card dragging

--- src/rmlui/game_screen.cpp
+++ src/rmlui/game_screen.cpp
@@ -8,6 +8,16 @@ GameScreen::GameScreen(Rml::Context* context)
 {
 	m_document = m_context->LoadDocument("ui/rml/game_screen.rml");
 	m_document->Show();
+
+	Rml::Element* cardContainer = m_document->GetElementById("card_container");
+
+	Rml::ElementList cards;
+	cardContainer->GetElementsByClassName(cards, "card-wrapper");
+
+	for (auto card : cards)
+	{
+		card->AddEventListener(Rml::EventId::Dragend, this);
+	}
 }
 
 void GameScreen::Destroy()
@@ -17,5 +27,16 @@ void GameScreen::Destroy()
 
 void GameScreen::ProcessEvent(Rml::Event& event)
 {
+	auto element = event.GetTargetElement();
+    if (!element) 
+		return;
 
+    if (event == Rml::EventId::Dragend)
+    {
+        float duration = 0.25f; 
+        Rml::Tween tween{Rml::Tween::Quadratic, Rml::Tween::Out};
+
+        element->Animate("left", Rml::Property(0.0f, Rml::Unit::PX), duration, tween);
+        element->Animate("top",  Rml::Property(0.0f, Rml::Unit::PX), duration, tween);
+    }
 }
